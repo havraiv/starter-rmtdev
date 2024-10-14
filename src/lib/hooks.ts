d2ae2dc29a39b603ceff2547093c5708dab75673
Iva Havranová <havranova.i@seznam.cz>
2024-10-14 17:06:51 +0200

    V-146 use react query

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,6 +1,27 @@
 import { useEffect, useState } from 'react';
-import { JobItem, JobItemExtended } from './types';
+import { JobItem } from './types';
 import { BASE_API_URL } from './constants';
+import { useQuery } from '@tanstack/react-query';
+
+export const useJobItem = (id: number | null) => {
+  const { data, isLoading } = useQuery(
+    ['job-item', id],
+    async () => {
+      const response = await fetch(`${BASE_API_URL}/${id}`);
+      const data = await response.json();
+      return data;
+    },
+    {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(id),
+      onError: () => {},
+    },
+  );
+  const jobItem = data?.jobItem;
+  return { jobItem, isLoading } as const;
+};
 
 export const useJobItems = (searchText?: string) => {
   const [jobItems, setJobItems] = useState<JobItem[]>([]);
@@ -24,6 +45,20 @@ export const useJobItems = (searchText?: string) => {
   return { jobItemsSliced, isLoading, jobItemsCount } as const;
 };
 
+export const useDebounce = <T>(value: T, time = 500): T => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, time);
+
+    return () => clearTimeout(timerId);
+  }, [value, time]);
+
+  return debouncedValue;
+};
+
 export const useActiveId = () => {
   const [activeId, setActiveId] = useState<number | null>(null);
 
@@ -42,39 +77,4 @@ export const useActiveId = () => {
   }, []);
 
   return activeId;
-};
-
-export const useJobItem = (id: number | null) => {
-  const [jobItem, setJobItem] = useState<JobItemExtended | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!id) return;
-
-    const fetchData = async () => {
-      setIsLoading(true);
-      const response = await fetch(`${BASE_API_URL}/${id}`);
-      const data = await response.json();
-      setIsLoading(false);
-      setJobItem(data.jobItem);
-    };
-
-    fetchData();
-  }, [id]);
-
-  return { jobItem, isLoading } as const;
-};
-
-export const useDebounce = <T>(value: T, time = 500): T => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
-
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setDebouncedValue(value);
-    }, time);
-
-    return () => clearTimeout(timerId);
-  }, [value, time]);
-
-  return debouncedValue;
 };
